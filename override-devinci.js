48d84de1ad42892c3a485086d1c7ea14d2f6d9b5
Matteo <matteo.courquin@gmail.com>
2021-09-17 12:43:41 +0200

    try Active JS

--- override-devinci.js
+++ override-devinci.js
@@ -1,5 +1,20 @@
+// document.getElementsByTagName('body')[0].classList.add('override');
+
 document.getElementsByTagName('body')[0].classList.add('override');
+
+btn = document.querySelector('.btn-css-recup');
+
 
 document.write('<h1>⚠ Mais où est le CSS ?!! ⚠</h1>');
 document.write('<img src="https://st.depositphotos.com/2224394/2351/i/600/depositphotos_23517607-stock-photo-business.jpg">');
+document.write('<button class="btn-css-recup">Récupérez le !</button>');
+
+
+function maFonction() {
+    body.classList.remove('override')
+}
+
+btn.addEventListener('click', maFonction)
+
+
 
