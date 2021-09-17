2bfab29abf0e20877d8e20a20de56cb02c5b02df
Matteo <matteo.courquin@gmail.com>
2021-09-17 13:04:40 +0200

    Done extension

--- override-devinci.js
+++ override-devinci.js
@@ -1,20 +1,17 @@
 // document.getElementsByTagName('body')[0].classList.add('override');
 
-document.getElementsByTagName('body')[0].classList.add('override');
+body = document.getElementsByTagName('body')[0].classList.add('override');
 
+
+document.write('<div><h1>⚠ Mais où est le CSS ?!! ⚠</h1><button class="btn-css-recup">Récupérez le !</button></div>');
+document.write('<img class="img-panique" src="https://st.depositphotos.com/2224394/2351/i/600/depositphotos_23517607-stock-photo-business.jpg">');
+
+img = document.querySelector('.img-panique');
 btn = document.querySelector('.btn-css-recup');
 
 
-document.write('<h1>⚠ Mais où est le CSS ?!! ⚠</h1>');
-document.write('<img src="https://st.depositphotos.com/2224394/2351/i/600/depositphotos_23517607-stock-photo-business.jpg">');
-document.write('<button class="btn-css-recup">Récupérez le !</button>');
+btn.addEventListener('click', function () {
+    document.write('<h2>Et non ! On va pas vous le rendre aussi facilement ;)</h2>');
+    img.style.display = "none";
 
-
-function maFonction() {
-    body.classList.remove('override')
-}
-
-btn.addEventListener('click', maFonction)
-
-
-
+});
